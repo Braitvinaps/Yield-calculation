4bde2ed42c830392e5efb6ee34ca7cbedbbdac61
Braitvinaps <britvinaps@gmail.com>
2021-11-03 14:46:38 +0300

    CSS

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ let btn = document.querySelector('button');
 
 function getTotal() {
     totalPills.value = +firstBoot.value + +secondBoot.value;
-    totalPills.innerHTML = `Получено таблеток общее B, <b>${totalPills.value.toFixed(1)}</b> кг`; 
+    totalPills.innerHTML = `Получено таблеток общее B, <span>${totalPills.value.toFixed(1)}</span> кг`; 
 
-    loss.innerHTML =  `Потери E, <b>${(+initMass.value - +totalPills.value - +reject.value - +assay.value).toFixed(4)}</b> кг`;
+    loss.innerHTML =  `Потери E, <span>${(+initMass.value - +totalPills.value - +reject.value - +assay.value).toFixed(4)}</span> кг`;
 
     amountTbl1.value = (+firstBoot.value * 1000000)/+tblWeight1.value;
     amountTbl2.value = (+secondBoot.value * 1000000)/+tblWeight2.value;
@@ -39,22 +39,22 @@ function getTotal() {
     maxTblWeight1.value = +tblWeight1.value + +StdDev1.value;
     maxTblWeight2.value = +tblWeight2.value + +StdDev2.value;
 
-    amountTbl1.innerHTML = `Колличество таблеток после стадии таблетирования K1, первая загрузка, <b>${amountTbl1.value.toFixed(0)}</b>  шт`;
-    amountTbl2.innerHTML = `Колличество таблеток после стадии таблетирования K1, вторая загрузка, <b>${amountTbl2.value.toFixed(0)}</b> шт`;
+    amountTbl1.innerHTML = `Колличество таблеток после стадии таблетирования K1, первая загрузка, <span>${amountTbl1.value.toFixed(0)}</span>  шт`;
+    amountTbl2.innerHTML = `Колличество таблеток после стадии таблетирования K1, вторая загрузка, <span>${amountTbl2.value.toFixed(0)}</span> шт`;
 
-    amountTbl.innerHTML = `Колличество таблеток после стадии таблетирования общее K, <b>${+amountTbl1.value.toFixed(0) + +amountTbl2.value.toFixed(0)}</b> шт`;
+    amountTbl.innerHTML = `Колличество таблеток после стадии таблетирования общее K, <span>${+amountTbl1.value.toFixed(0) + +amountTbl2.value.toFixed(0)}</span> шт`;
 
-    yield.innerHTML = `Выход, <b>${((+totalPills.value/+initMass.value)*100).toFixed(0)}</b> %`;
+    yield.innerHTML = `Выход, <span>${((+totalPills.value/+initMass.value)*100).toFixed(0)}</span> %`;
     
-    minTblWeight1.innerHTML = `Min масса таблетки H1, первая загрузка, <b>${minTblWeight1.value.toFixed(1)}</b> мг`;
-    minTblWeight2.innerHTML = `Min масса таблетки H1, вторая загрузка, <b>${minTblWeight2.value.toFixed(1)}</b> мг`;
-    maxTblWeight1.innerHTML = `Max масса таблетки I1, первая загрузка, <b>${maxTblWeight1.value.toFixed(1)}</b> мг`;
-    maxTblWeight2.innerHTML = `Max масса таблетки I2, вторая загрузка, <b>${maxTblWeight2.value.toFixed(1)}</b> мг`;
+    minTblWeight1.innerHTML = `Min масса таблетки H1, первая загрузка, <span>${minTblWeight1.value.toFixed(1)}</span> мг`;
+    minTblWeight2.innerHTML = `Min масса таблетки H1, вторая загрузка, <span>${minTblWeight2.value.toFixed(1)}</span> мг`;
+    maxTblWeight1.innerHTML = `Max масса таблетки I1, первая загрузка, <span>${maxTblWeight1.value.toFixed(1)}</span> мг`;
+    maxTblWeight2.innerHTML = `Max масса таблетки I2, вторая загрузка, <span>${maxTblWeight2.value.toFixed(1)}</span> мг`;
     
-    minAmountTbl1.innerHTML = `Колличество таблеток с Min массой Y1, первая загрузка, <b>${((+firstBoot.value * 1000000)/+minTblWeight1.value).toFixed(0)}</b> шт`;
-    minAmountTbl2.innerHTML = `Колличество таблеток с Min массой Y2, вторая загрузка, <b>${((+secondBoot.value * 1000000)/+minTblWeight2.value).toFixed(0)}</b> шт`;
-    maxAmountTbl1.innerHTML = `Колличество таблеток с Max массой Z1, первая загрузка, <b>${((+firstBoot.value * 1000000)/+maxTblWeight1.value).toFixed(0)}</b> шт`;
-    maxAmountTbl2.innerHTML = `Колличество таблеток с Max массой Z2, вторая загрузка, <b>${((+secondBoot.value * 1000000)/+maxTblWeight2.value).toFixed(0)}</b> шт`;
+    minAmountTbl1.innerHTML = `Колличество таблеток с Min массой Y1, первая загрузка, <span>${((+firstBoot.value * 1000000)/+minTblWeight1.value).toFixed(0)}</span> шт`;
+    minAmountTbl2.innerHTML = `Колличество таблеток с Min массой Y2, вторая загрузка, <span>${((+secondBoot.value * 1000000)/+minTblWeight2.value).toFixed(0)}</span> шт`;
+    maxAmountTbl1.innerHTML = `Колличество таблеток с Max массой Z1, первая загрузка, <span>${((+firstBoot.value * 1000000)/+maxTblWeight1.value).toFixed(0)}</span> шт`;
+    maxAmountTbl2.innerHTML = `Колличество таблеток с Max массой Z2, вторая загрузка, <span>${((+secondBoot.value * 1000000)/+maxTblWeight2.value).toFixed(0)}</span> шт`;
 }
 
 btn.addEventListener('click', getTotal);
